0cb2b5150bd0e2d0a99125cf81558529e5d6abf8
Mo Binni <mobinni@hotmail.com>
2016-12-01 16:13:14 -0500

    fixed menu style guide

--- utils/dependencies.js
+++ utils/dependencies.js
@@ -3,7 +3,7 @@ var cpx = require('cpx');
 
 const files = {
     'src/main.js': 'www',
-    'src/tools/prism.js': 'www',
+    'src/tools/prism.js': 'www/tools',
     'src/material-design-lite/material.min.js': 'www/material-design-lite',
     'src/material-design-lite/material.min.css': 'www/material-design-lite',
     'src/images/**/*': 'www/images'
