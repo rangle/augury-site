e67a291535c6185da6ba667ed0fbd3a7648b5ec7
Mo Binni <mobinni@hotmail.com>
2016-12-02 11:00:05 -0500

    fixes for PR in regards to vendors folder being setup

--- utils/dependencies.js
+++ utils/dependencies.js
@@ -1,11 +1,16 @@
-var fs = require('fs-extra');
-var cpx = require('cpx');
+const fs = require('fs-extra'),
+    cpx = require('cpx'),
+    chalk = require('chalk');
+
+const message = chalk.red.bold('Be careful moving around the folder structure, make sure all the necessary files are always copied');
+console.log(message);
+
 
 const files = {
     'src/main.js': 'www',
     'src/tools/prism.js': 'www/tools',
-    'src/material-design-lite/material.min.js': 'www/material-design-lite',
-    'src/material-design-lite/material.min.css': 'www/material-design-lite',
+    'src/vendors/material-design-lite/material.min.js': 'www/vendors/material-design-lite',
+    'src/vendors/material-design-lite/material.min.css': 'www/vendors/material-design-lite',
     'src/images/**/*': 'www/images',
     'src/demo/*': 'www/demo'
 };
@@ -14,5 +19,16 @@ Object.keys(files).forEach(copy);
 
 function copy(file) {
     const dest = files[file];
-    cpx.copy(file, dest);
+
+    const s1 = chalk.green.bold(file),
+        s2 = chalk.green.bold(dest);
+
+    console.log(chalk.blue.bold(`Copying ${s1} to ${s2}`));
+
+    cpx.copy(file, dest, {}, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
 }
+
